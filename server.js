e1ec06e64486490c68c40c58f63eba120ac42d7d
Andreja <andrejagit23@gmail.com>
2024-12-11 06:57:11 +0100

    Adding api prefix for grouping all API routes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const authRoutes = require('./routes/auth');
 
 app.use(cors());
 app.use(express.json());
-app.use('/auth', authRoutes);
+app.use('/api/auth', authRoutes);
 
 
 app.get('/', (req, res) => {
